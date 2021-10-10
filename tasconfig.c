160dd62ba47b1df2dc5a2a624358f80cfba87327
Adrian Kennard <a@k.gg>
2021-10-10 08:52:34 +0100

    More

--- tasconfig.c
+++ tasconfig.c
@@ -7,6 +7,7 @@
 #include <sys/time.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <unistd.h>
 #include <err.h>
 #include <sqllib.h>
 #include <ajl.h>
@@ -28,6 +29,7 @@ int main(int argc, const char *argv[])
    const char *mqttid = NULL;
    int all = 0;
    int info = 0;
+   int backup = 0;
 
    poptContext optCon;          // context for parsing command-line options
    const struct poptOption optionsTable[] = {
@@ -43,6 +45,7 @@ int main(int argc, const char *argv[])
       { "mqtt-password", 'p', POPT_ARG_STRING, &mqttpassword, 0, "MQTT password", "password" },
       { "mqtt-id", 0, POPT_ARG_STRING, &mqttid, 0, "MQTT id", "id" },
       { "info", 0, POPT_ARG_NONE, &info, 0, "Show changes", NULL },
+      { "backup", 0, POPT_ARG_NONE, &backup, 0, "Backup device", NULL },
       { "all", 0, POPT_ARG_NONE, &all, 0, "All devices", NULL },
       POPT_AUTOHELP { }
    };
@@ -64,6 +67,8 @@ int main(int argc, const char *argv[])
    const char *topic = NULL;    // Current topic / device
    char *foundtopic = NULL;
    char *foundpayload = NULL;
+   char *name = NULL;
+   char *value = NULL;
    int foundpayloadlen = 0;
    int e = mosquitto_lib_init();
    if (e)
@@ -81,26 +86,63 @@ int main(int argc, const char *argv[])
       e = mosquitto_subscribe(mqtt, NULL, "stat/#", 0);
       if (e)
          errx(1, "MQTT subscribe failed %s", mosquitto_strerror(e));
+      if (backup && all)
+      {
+         e = mosquitto_subscribe(mqtt, NULL, "tele/#", 0);
+         if (e)
+            errx(1, "MQTT subscribe failed %s", mosquitto_strerror(e));
+      }
    }
    void disconnect(struct mosquitto *mqtt, void *obj, int rc) {
       obj = obj;
       rc = rc;
    }
+   struct found_s {
+      struct found_s *next;
+      char *topic;
+   } *found = NULL;
+   int finding = (backup && all);
    void message(struct mosquitto *mqtt, void *obj, const struct mosquitto_message *msg) {
+      if (sqldebug)
+         warnx("< %s %.*s", msg->topic, msg->payloadlen, (char *) msg->payload ? : "");
       obj = obj;
-      if (foundtopic)
+      // Expecting stat/[name]/RESULT {"name"...
+      // Check topic
+      char *s = msg->topic;
+      if (finding && !strncmp(s, "tele/", 5))
+      {                         // Add to list
+         char *t = s + 5;
+         char *d = strchr(t, '/');
+         if (d && !strcmp(d, "/LWT") && msg->payloadlen == 6 && !strncmp((char *) msg->payload, "Online", 6))
+         {
+            *d = 0;
+            struct found_s *f = malloc(sizeof(*f));
+            f->topic = strdup(t);
+            f->next = found;
+            found = f;
+         }
+      }
+      if (foundtopic || !name)
          return;
-      char *s = strchr(msg->topic, '/');
-      if (!s)
+      if (strncmp(s, "stat/", 5))
          return;
-      s++;
+      s += 5;
       if (strncmp(s, topic, strlen(topic)))
          return;
       s += strlen(topic);
       if (*s != '/')
          return;
-      if (sqldebug)
-         warnx("%s: %.*s", msg->topic, msg->payloadlen, (char *) msg->payload ? : "");
+      s++;
+      if (strcmp(s, "RESULT"))
+         return;
+      // Check payload
+      j_t j = j_create();
+      const char *je = j_read_mem(j, (char *) msg->payload, msg->payloadlen);
+      if (!je && !j_find(j, name))
+         je = "nope";
+      j_delete(&j);
+      if (je)
+         return;
       foundtopic = strdup(msg->topic);
       if (!foundtopic)
          errx(1, "malloc");
@@ -118,23 +160,32 @@ int main(int argc, const char *argv[])
    e = mosquitto_connect(mqtt, mqtthostname, 1883, 60);
    if (e)
       errx(1, "MQTT connect failed (%s) %s", mqtthostname, mosquitto_strerror(e));
+   e = mosquitto_loop_start(mqtt);
+   if (e)
+      errx(1, "MQTT loop failed (%s) %s", mqtthostname, mosquitto_strerror(e));
+
    sql_real_connect(&sql, sqlhostname, sqlusername, sqlpassword, sqldatabase, 0, NULL, 0, 1, sqlconffile);
 
-   int getstat(void) {          // Wait for a message from current topic device...
+   void sendfree(char *topic, int len, void *payload) {
       free(foundtopic);
       foundtopic = NULL;
       free(foundpayload);
       foundpayload = NULL;
+      if (sqldebug)
+         warnx("> %s %.*s", topic, len, (char *) payload ? : "");
+      int e = mosquitto_publish(mqtt, NULL, topic, len, payload, 0, 0);
+      if (e)
+         errx(1, "MQTT publish failed %s", mosquitto_strerror(e));
+      free(topic);
+   }
+
+   int getstat(void) {          // Wait for a message from current topic device...
       struct timeval now;
       gettimeofday(&now, NULL);
-      long long giveup = now.tv_sec * 1000000 + now.tv_usec + 1000000;
+      long long giveup = now.tv_sec * 1000000 + now.tv_usec + 2000000;
       while (1)
       {
-         e = mosquitto_loop(mqtt, 1000, 1);
-         if (e)
-            errx(1, "MQTT loop failed %s", mosquitto_strerror(e));
-         if (e)
-            return e;
+         usleep(10000);
          if (foundtopic)
             return 0;
          gettimeofday(&now, NULL);
@@ -146,13 +197,11 @@ int main(int argc, const char *argv[])
 
    void config(SQL_RES * res) { // Configure a devices
       // Check device is on line even...
+      name = "Topic";
       char *t = NULL;
-      if (asprintf(&t, "cmnd/%s/status", topic) < 0)
+      if (asprintf(&t, "cmnd/%s/%s", topic, name) < 0)
          errx(1, "malloc");
-      int e = mosquitto_publish(mqtt, NULL, t, 0, NULL, 0, 0);
-      if (e)
-         errx(1, "MQTT publish failed %s", mosquitto_strerror(e));
-      free(t);
+      sendfree(t, 0, NULL);
       if (getstat())
       {
          char *alt = sql_colz(res, "_Topic");
@@ -162,10 +211,7 @@ int main(int argc, const char *argv[])
             topic = alt;
             if (asprintf(&t, "cmnd/%s/status", topic) < 0)
                errx(1, "malloc");
-            int e = mosquitto_publish(mqtt, NULL, t, 0, NULL, 0, 0);
-            if (e)
-               errx(1, "MQTT publish failed %s", mosquitto_strerror(e));
-            free(t);
+            sendfree(t, 0, NULL);
             if (getstat())
             {
                warnx("Not responding %s", topic);
@@ -182,45 +228,61 @@ int main(int argc, const char *argv[])
       else if (info)
          fprintf(stderr, "Checking %s\n", topic);
       // Check settings
-      const char *name = NULL,
-          *value = NULL;
       const char *findval(j_t j) {      // Get value, including special cases e.g. for rules
+         const char *v = j_get(j, name);
          if (!strncmp(name, "Rule", 4) && isdigit(name[4]))
          {
+            if (v)
+               return j_get(j, "Rules");        // Alternative style...
             j_t r = j_find(j, name);
             if (r)
                return j_get(r, "Rules");
             return NULL;
          }
-         const char *v = j_get(j, name);
+         if (!strncmp(name, "PulseTime", 9) && isdigit(name[9]))
+         {
+            j_t r = j_find(j, name);
+            if (r)
+               return j_get(r, "Set");
+            return NULL;
+         }
+         if (!v)
+            return NULL;
+         if (!strcmp(name, "Webserver"))
+         {
+            if (strstr(v, "Active for Admin"))
+               return "2";
+            if (strstr(v, "Active for User"))
+               return "1";
+            return "0";
+         }
          if (!strncmp(name, "SetOption", 9))
          {
             if (!strcmp(v, "ON"))
                return "1";
             if (!strcmp(v, "OFF"))
                return "0";
+            return v;
          }
          return v;
       }
+      sql_string_t s = { };
+      if (backup)
+         sql_sprintf(&s, "UPDATE `%#S` SET ", sqltable);
       for (int n = 0; n < res->field_count; n++)
-         if ((value = res->current_row[n]) && *(name = res->fields[n].name) != '_')
+         if (((value = res->current_row[n]) || backup) && *(name = res->fields[n].name) != '_')
          {
-            if (!*value)
-               value = "0";     // Cannot be actually blank
             char *t = NULL;
             if (asprintf(&t, "cmnd/%s/%s", topic, name) < 0)
                errx(1, "malloc");
-            int e = mosquitto_publish(mqtt, NULL, t, 0, NULL, 0, 0);
-            if (e)
-               errx(1, "publish failed %s", mosquitto_strerror(e));
-            free(t);
+            sendfree(t, 0, NULL);
             if (getstat())
             {
                warnx("No data for %s/%s", topic, name);
                continue;
             }
             char match = 0;
-            const char *v = "unknown";
+            const char *v = NULL;
             j_t j = j_create();
             const char *je = j_read_mem(j, foundpayload, foundpayloadlen);
             if (je)
@@ -230,57 +292,87 @@ int main(int argc, const char *argv[])
                v = findval(j);
                if (!v)
                   warnx("Not found %s in (%s)", name, foundpayload);
-               else if (!strcmp(v, value))
+               else if ((!value || !*value || !strcmp(value, "0")) && (!v || !*v || !strcmp(v, "0")))
+                  match = 1;
+               else if (v && value && !strcmp(v, value))
                   match = 1;
             }
             if (!match)
-            {                   // Send
-               if (asprintf(&t, "cmnd/%s/%s", topic, name) < 0)
-                  errx(1, "malloc");
-               int e = mosquitto_publish(mqtt, NULL, t, strlen(value), value, 0, 0);
-               if (e)
-                  errx(1, "publish failed %s", mosquitto_strerror(e));
-               free(t);
-               if (getstat())
-                  warnx("No response setting %s to %s on %s", name, value, topic);
-               else
-               {
-                  j_t j2 = j_create();
-                  const char *je = j_read_mem(j2, foundpayload, foundpayloadlen);
-                  if (je)
-                     warnx("Bad JSON: %s (%s)", foundpayload, je);
+            {                   // Send / store
+               if (backup)
+               {                // Store
+                  if (v && !*v)
+                     v = NULL;
+                  if (v || value)
+                     sql_sprintf(&s, "`%#S`=%#s,", name, v);
+               } else
+               {                // Correct
+                  if (asprintf(&t, "cmnd/%s/%s", topic, name) < 0)
+                     errx(1, "malloc");
+                  sendfree(t, strlen(value), value);
+                  if (getstat())
+                     warnx("No response setting %s to %s on %s", name, value, topic);
                   else
                   {
-                     const char *v2 = findval(j2);
-                     if (!v2)
-                        warnx("Not found %s in (%s)", name, topic);
-                     else if (strcmp(v2, value))
-                        warnx("Unable to set %s to %s on %s (is %s)", name, value, topic, v2);
+                     j_t j2 = j_create();
+                     const char *je = j_read_mem(j2, foundpayload, foundpayloadlen);
+                     if (je)
+                        warnx("Bad JSON: %s (%s)", foundpayload, je);
                      else
                      {
-                        if (info)
-                           fprintf(stderr, "Updated %s to %s on %s (was %s)\n", name, value, topic, v);
-                        if (!strncmp(name, "Rule", 4) && isdigit(name[4]) && strcmp(value, "0"))
-                        {       // Special case for Rule setting
-                           if (asprintf(&t, "cmnd/%s/%s", topic, name) < 0)
-                              errx(1, "malloc");
-                           int e = mosquitto_publish(mqtt, NULL, t, 1, "1", 0, 0);
-                           if (e)
-                              errx(1, "publish failed %s", mosquitto_strerror(e));
-                           free(t);
-                           if (getstat())
-                              warnx("Setting %s 1 did not work on %s", name, topic);
+                        const char *v2 = findval(j2);
+                        if (!v2)
+                           warnx("Not found %s in (%s)", name, topic);
+                        else if (strcmp(v2, value))
+                           warnx("Unable to set %s to %s on %s (is %s)", name, value, topic, v2);
+                        else
+                        {
+                           if (info)
+                              fprintf(stderr, "Updated %s to %s on %s (was %s)\n", name, value, topic, v ? : "unknown");
+                           if (!strncmp(name, "Rule", 4) && isdigit(name[4]) && strcmp(value, "0"))
+                           {    // Special case for Rule setting
+                              if (asprintf(&t, "cmnd/%s/%s", topic, name) < 0)
+                                 errx(1, "malloc");
+                              sendfree(t, 1, "1");
+                              if (getstat())
+                                 warnx("Setting %s 1 did not work on %s", name, topic);
+                           }
                         }
                      }
+                     j_delete(&j2);
                   }
-                  j_delete(&j2);
                }
             }
             j_delete(&j);
          }
+      if (backup)
+      {
+         if (sql_back_s(&s) == ',')
+         {
+            sql_sprintf(&s, " WHERE `Topic`=%#s", sql_colz(res, "Topic"));
+            sql_safe_query_s(&sql, &s);
+         } else
+            sql_free_s(&s);
+      }
+   }
+   void configtopic(void) {
+      SQL_RES *res = sql_safe_query_store_free(&sql, sql_printf("SELECT * FROM `%#S` WHERE `Topic`=%#s", sqltable, topic));
+      if (!sql_fetch_row(res))
+      {
+         warnx("Not found in %s table: %s", sqltable, topic);
+         sql_free_result(res);
+         if (!backup)
+            return;
+         sql_safe_query_free(&sql, sql_printf("INSERT INTO `%#S` SET `Topic`=%#s", sqltable, topic));
+         res = sql_safe_query_store_free(&sql, sql_printf("SELECT * FROM `%#S` WHERE `Topic`=%#s", sqltable, topic));
+         if (!sql_fetch_row(res))
+            errx(1, "create failed");
+      }
+      config(res);
+      sql_free_result(res);
    }
 
-   if (all)
+   if (all&&!backup)
    {                            // All
       SQL_RES *res = sql_safe_query_store_free(&sql, sql_printf("SELECT * FROM `%#S`", sqltable));
       while (sql_fetch_row(res))
@@ -290,18 +382,26 @@ int main(int argc, const char *argv[])
       }
       sql_free_result(res);
    }
-   {                            // Args
-      while ((topic = poptGetArg(optCon)))
+   if (all && backup)
+   {
+      sleep(1);                 // Get LWTs in...
+      finding = 0;
+      while (found)
       {
-         SQL_RES *res = sql_safe_query_store_free(&sql, sql_printf("SELECT * FROM `%#S` WHERE `Topic`=%#s", sqltable, topic));
-         if (sql_fetch_row(res))
-            config(res);
-         else
-            warnx("Not found in %s table: %s", sqltable, topic);
-         sql_free_result(res);
+         struct found_s *f = found;
+         found = f->next;
+         topic = f->topic;
+         configtopic();
+         free(f->topic);
+         free(f);
       }
    }
+   {                            // Args
+      while ((topic = poptGetArg(optCon)))
+         configtopic();
+   }
    // Tidy up
+   mosquitto_loop_stop(mqtt, 1);
    free(foundtopic);
    free(foundpayload);
    mosquitto_destroy(mqtt);
